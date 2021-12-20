861417ebd5dcc1883e7a85e66df9681c77d3d5c0
Jan Kvetina <jan.kvetina@gmail.com>
2021-12-20 08:45:51 +0100

    Cleanup

--- views/nav_pages_to_add.sql
+++ views/nav_pages_to_add.sql
@@ -6,10 +6,11 @@ SELECT
     p.page_alias,
     p.page_name,
     p.page_title,
-    p.page_id               AS order#,          -- find nearest page in the same group (below/above)
+    NULL                    AS order#,
     p.page_css_classes      AS css_class,
     --
-    'Y'                     AS is_hidden,       -- dont show on default
+    'Y'                     AS is_hidden,       -- hide page on default
+    'Y'                     AS is_reset,        -- reset page items
     --
     p.page_group,
     p.page_id               AS page_link,
@@ -19,17 +20,16 @@ LEFT JOIN navigation n
     ON n.app_id             = p.application_id
     AND n.page_id           = p.page_id
 LEFT JOIN (
-    -- find common parent in the same group
+    -- find parent in the same group
     SELECT
         p.page_group,
         NVL(MIN(n.parent_id), MAX(n.page_id)) AS parent_id
     FROM navigation n
     JOIN apex_application_pages p
-        ON p.application_id = n.app_id
-        AND p.page_id       = n.page_id
+        ON p.application_id     = n.app_id
+        AND p.page_id           = n.page_id
     GROUP BY p.page_group
 ) g
     ON g.page_group         = p.page_group
 WHERE p.application_id      = app.get_app_id()
-    AND p.page_id           BETWEEN 1 and 9999
     AND n.page_id           IS NULL;
--- views/nav_pages_to_remove.sql
+++ views/nav_pages_to_remove.sql
@@ -5,5 +5,4 @@ LEFT JOIN apex_application_pages p
     ON p.application_id     = n.app_id
     AND p.page_id           = n.page_id
 WHERE n.app_id              = app.get_app_id()
-    AND n.page_id           BETWEEN 1 AND 9999
     AND p.application_id    IS NULL;
