ef70a94866cb36aeae7100ae6b32711e9a5950b8
Jan Kvetina <jan.kvetina@gmail.com>
2021-12-25 23:23:40 +0100

    Fix high business event count

--- views/sessions_overview.sql
+++ views/sessions_overview.sql
@@ -34,6 +34,7 @@ b AS (
         ON l.created_at     >= s.today
         AND l.created_at    < s.today + 1
         AND s.app_id        = l.app_id
+        AND s.session_id    = l.session_id
     GROUP BY l.session_id
 )
 SELECT
