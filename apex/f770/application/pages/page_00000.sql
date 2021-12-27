7152957fefc09831ef5e8646c0daa02901d3cd3e
Jan Kvetina <jan.kvetina@gmail.com>
2021-12-27 20:04:44 +0100

    Hide Edit buttons in grid, make Save green on change

--- apex/f770/application/pages/page_00000.sql
+++ apex/f770/application/pages/page_00000.sql
@@ -20,7 +20,25 @@ wwv_flow_api.create_page(
 ,p_page_template_options=>'#DEFAULT#'
 ,p_protection_level=>'D'
 ,p_last_updated_by=>'DEV'
-,p_last_upd_yyyymmddhh24miss=>'20211225131653'
+,p_last_upd_yyyymmddhh24miss=>'20211227152238'
+);
+wwv_flow_api.create_page_da_event(
+ p_id=>wwv_flow_api.id(11425112951377811)
+,p_name=>'ON_LOAD'
+,p_event_sequence=>10
+,p_bind_type=>'bind'
+,p_bind_event_type=>'ready'
+);
+wwv_flow_api.create_page_da_action(
+ p_id=>wwv_flow_api.id(11425241361377812)
+,p_event_id=>wwv_flow_api.id(11425112951377811)
+,p_event_result=>'TRUE'
+,p_action_sequence=>10
+,p_execute_on_page_init=>'Y'
+,p_action=>'NATIVE_JAVASCRIPT_CODE'
+,p_attribute_01=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'apex_page_loaded();',
+''))
 );
 wwv_flow_api.component_end;
 end;
