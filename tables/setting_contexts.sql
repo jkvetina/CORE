3c3a0396f1c70f4d7ac7a192bec733a2475cc3bc
Jan Kvetina <jan.kvetina@gmail.com>
2021-12-30 00:29:29 +0100

    Prep for dynamic pivot for context settings

---
+++ tables/setting_contexts.sql
@@ -0,0 +1,28 @@
+--DROP TABLE setting_contexts PURGE;
+CREATE TABLE setting_contexts (
+    app_id              NUMBER(4)       CONSTRAINT nn_setting_contexts_app_id   NOT NULL,
+    context_id          VARCHAR2(64)    CONSTRAINT nn_setting_contexts_name     NOT NULL,
+    context_name        VARCHAR2(64),
+    description_        VARCHAR2(1000),
+    order#              NUMBER(4),
+    --
+    updated_by          VARCHAR2(30),
+    updated_at          DATE,
+    --
+    CONSTRAINT uq_setting_contexts
+        UNIQUE (app_id, context_id),
+    --
+    CONSTRAINT fk_setting_contexts_app_id
+        FOREIGN KEY (app_id)
+        REFERENCES apps (app_id)
+)
+STORAGE (BUFFER_POOL KEEP);
+--
+COMMENT ON TABLE  setting_contexts                      IS 'List of setting_contexts shared through whole app';
+--
+COMMENT ON COLUMN setting_contexts.app_id               IS 'Application ID';
+COMMENT ON COLUMN setting_contexts.context_id           IS 'To allow multiple values depending on context value';
+COMMENT ON COLUMN setting_contexts.context_name         IS 'Friendly name';
+COMMENT ON COLUMN setting_contexts.description_         IS 'Description';
+COMMENT ON COLUMN setting_contexts.order#               IS 'Order for sorting purposes';
+
