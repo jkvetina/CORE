1a0175752563e24efae203eba80d8bf9c351c426
Jan Kvetina <jan.kvetina@gmail.com>
2022-01-12 21:38:38 +0100

    Add Jobs link

--- apex/f770/application/pages/page_00950.sql
+++ apex/f770/application/pages/page_00950.sql
@@ -75,6 +75,7 @@ wwv_flow_api.create_page_plug(
 '        WHEN ''TRIGGER''      THEN app.get_page_link(952)',
 '        WHEN ''VIEW''         THEN app.get_page_link(955)',
 '        WHEN ''PACKAGE''      THEN app.get_page_link(960)',
+'        WHEN ''JOB''          THEN app.get_page_link(905)',
 '        ELSE NULL',
 '        END AS page_link',
 'FROM user_objects',
