0b8d918b8652a3a416c57995ac3b8eee67beeb0a
Jan Kvetina <jan.kvetina@gmail.com>
2022-01-15 15:39:04 +0100

    Remove non standard template

--- apex/f770/application/pages/page_00951.sql
+++ apex/f770/application/pages/page_00951.sql
@@ -19,7 +19,6 @@ wwv_flow_api.create_page(
 ,p_step_title=>'Tables'
 ,p_autocomplete_on_off=>'OFF'
 ,p_group_id=>wwv_flow_api.id(15841923064543077)
-,p_step_template=>wwv_flow_api.id(9463753933679086)
 ,p_page_template_options=>'#DEFAULT#'
 ,p_required_role=>wwv_flow_api.id(9556407311505078)
 ,p_last_updated_by=>'DEV'
