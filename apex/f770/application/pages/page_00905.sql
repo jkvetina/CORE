b35175d1d2aa5347eaed23b71cb93265b7e0d101
Jan Kvetina <jan.kvetina@gmail.com>
2022-02-06 10:54:56 +0100

    Renamed Schedulers page to Jobs

--- apex/f770/application/pages/page_00905.sql
+++ apex/f770/application/pages/page_00905.sql
@@ -14,15 +14,15 @@ wwv_flow_api.component_begin (
 wwv_flow_api.create_page(
  p_id=>905
 ,p_user_interface_id=>wwv_flow_api.id(9169746885570061)
-,p_name=>'#fa-server-play Schedulers'
-,p_alias=>'SCHEDULERS'
-,p_step_title=>'Schedulers'
+,p_name=>'#fa-server-play Jobs'
+,p_alias=>'JOBS'
+,p_step_title=>'Jobs'
 ,p_autocomplete_on_off=>'OFF'
 ,p_group_id=>wwv_flow_api.id(9240371448352386)
 ,p_page_template_options=>'#DEFAULT#'
 ,p_required_role=>wwv_flow_api.id(9556407311505078)
 ,p_last_updated_by=>'DEV'
-,p_last_upd_yyyymmddhh24miss=>'20220129092907'
+,p_last_upd_yyyymmddhh24miss=>'20220206093910'
 );
 wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(11425322549377813)
@@ -1248,6 +1248,7 @@ wwv_flow_api.create_page_plug(
 ,p_plug_display_sequence=>30
 ,p_include_in_reg_disp_sel_yn=>'Y'
 ,p_plug_display_point=>'BODY'
+,p_plug_source=>'Scheduled jobs.'
 ,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
 ,p_attribute_01=>'N'
 ,p_attribute_02=>'HTML'
