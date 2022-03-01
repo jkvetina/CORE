152b2547809d4c131edcaceb7a7b80524d4a29e2
Jan Kvetina <jan.kvetina@gmail.com>
2022-03-01 23:16:07 +0100

    List of unused items

---
+++ views/translations_unused.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE VIEW translations_unused AS
+WITH x AS (
+    SELECT /*+ MATERIALIZE */
+        app.get_app_id()            AS app_id,
+        app.get_item('$PAGE_ID')    AS page_id
+    FROM DUAL
+)
+SELECT
+    REGEXP_SUBSTR(t.item_name, '^([^_]+)', 1, 1, NULL, 1) AS item_type,
+    --
+    t.item_name,
+    t.page_id,
+    t.value_en
+FROM translated_items t
+CROSS JOIN x
+LEFT JOIN translations_extracts d
+    ON d.page_id        = t.page_id
+    AND d.item_name     = t.item_name
+WHERE t.app_id          = x.app_id
+    AND t.page_id       = NVL(x.page_id, t.page_id)
+    AND d.item_name     IS NULL;
+
