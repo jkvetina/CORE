5f072e710d30726ec484cb1eafee50cc3c49a51b
Jan Kvetina <jan.kvetina@gmail.com>
2022-03-09 21:26:54 +0100

    Improve performance

--- views/nav_availability_mvw.sql
+++ views/nav_availability_mvw.sql
@@ -3,34 +3,42 @@ CREATE MATERIALIZED VIEW nav_availability_mvw
 BUILD DEFERRED
 REFRESH ON DEMAND COMPLETE
 AS
+WITH w AS (
+    SELECT a.owner, a.application_id
+    FROM apex_applications a
+    WHERE a.owner NOT LIKE 'APEX%'
+)
 SELECT
     p.application_id,
     p.page_id,
-    MIN(p.authorization_scheme)     AS auth_scheme,
-    MIN(f.package_name)             AS package_name,
-    MIN(f.object_name)              AS procedure_name,
-    MIN(f.pls_type)                 AS data_type,
-    MIN(a.argument_name)            AS argument_name            
+    MIN(p.authorization_scheme) AS auth_scheme,
+    --
+    MIN(CASE WHEN a.position = 0 THEN a.package_name    END) AS package_name,
+    MIN(CASE WHEN a.position = 0 THEN a.object_name     END) AS procedure_name,
+    MIN(CASE WHEN a.position = 0 THEN a.pls_type        END) AS data_type,
+    MIN(CASE WHEN a.position = 1 THEN a.argument_name   END) AS argument_name
 FROM apex_application_pages p
+JOIN w
+    ON w.application_id             = p.application_id
 LEFT JOIN all_procedures s
-    ON s.owner                      = app.get_core_owner()
+    ON s.owner                      = w.owner
     AND s.object_name               IN ('A' || TO_CHAR(p.application_id), 'APP', 'AUTH')   -- packages
     AND s.procedure_name            = p.authorization_scheme
-LEFT JOIN all_arguments f
-    ON f.owner                      = s.owner
-    AND f.object_name               = s.procedure_name
-    AND f.package_name              = s.object_name
-    AND f.overload                  IS NULL
-    AND f.position                  = 0
-    AND f.argument_name             IS NULL
-    AND f.in_out                    = 'OUT'
 LEFT JOIN all_arguments a
-    ON a.owner                      = f.owner
-    AND a.object_name               = f.package_name
-    AND a.package_name              = f.object_name
+    ON a.owner                      = s.owner
+    AND a.object_name               = s.procedure_name
+    AND a.package_name              = s.object_name
     AND a.overload                  IS NULL
-    AND a.position                  = 1
-    AND a.data_type                 = 'NUMBER'
-    AND a.in_out                    = 'IN'
+    AND ((
+            a.position              = 0
+            AND a.argument_name     IS NULL
+            AND a.in_out            = 'OUT'
+        )
+        OR (
+            a.position              = 1
+            AND a.data_type         = 'NUMBER'
+            AND a.in_out            = 'IN'
+        )
+    )
 GROUP BY p.application_id, p.page_id;
 
