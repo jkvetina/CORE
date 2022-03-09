5eac9192b06e6a74bd159345331735ec6f23c439
Jan Kvetina <jan.kvetina@gmail.com>
2022-03-09 22:40:10 +0100

    Fix view columns

--- views/obj_view_columns.sql
+++ views/obj_view_columns.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE VIEW obj_view_columns AS
 WITH x AS (
     SELECT /*+ MATERIALIZE */
-        app.get_item('$VIEW_NAME') AS view_name,
+        app.get_owner()                             AS owner,
+        app.get_item('$VIEW_NAME')                  AS view_name,
         --
         UPPER(app.get_item('$SEARCH_VIEWS'))        AS search_views,
         UPPER(app.get_item('$SEARCH_COLUMNS'))      AS search_columns
@@ -36,31 +37,34 @@ c AS (
         CASE WHEN c.column_name LIKE x.search_columns   || '%' ESCAPE '\'   THEN 'Y' END AS is_found_column
         --CASE WHEN c.data_type   LIKE x.search_data_type || '%' ESCAPE '\'   THEN 'Y' END AS is_found_data_type,
         --CASE WHEN NVL(c.data_precision, c.data_length) = x.search_size      THEN 'Y' END AS is_found_size
-    FROM user_tab_columns c
-    JOIN user_views t
-        ON t.view_name         = c.table_name
-    CROSS JOIN x
-    WHERE t.view_name          = NVL(x.view_name, t.view_name)
-        AND t.view_name        != app.get_dml_table(t.view_name)
-        AND (c.table_name      LIKE '%' || x.search_views || '%' ESCAPE '\' OR x.search_views IS NULL)
+    FROM all_tab_columns c
+    JOIN x
+        ON x.owner          = c.owner
+    JOIN all_views t
+        ON t.owner          = c.owner
+        AND t.view_name     = c.table_name
+    WHERE t.view_name       = NVL(x.view_name, t.view_name)
+        AND t.view_name     != app.get_dml_table(t.view_name)
+        AND (c.table_name   LIKE '%' || x.search_views || '%' ESCAPE '\' OR x.search_views IS NULL)
 )
 SELECT
     c.view_name,
     c.column_id,
     c.column_name,
-    c.column_name       AS column_name_old,
+    c.column_name           AS column_name_old,
     c.data_type,
     c.data_default,
-    c.avg_col_len       AS avg_length,
+    c.avg_col_len           AS avg_length,
     --
     CASE WHEN c.nullable = 'N' THEN 'Y' END AS is_nn,
     --
     m.comments
 FROM c
 CROSS JOIN x
-LEFT JOIN user_col_comments m
-    ON m.table_name     = c.view_name
-    AND m.column_name   = c.column_name
+LEFT JOIN all_col_comments m
+    ON m.owner              = x.owner
+    AND m.table_name        = c.view_name
+    AND m.column_name       = c.column_name
 WHERE 1 = 1
     AND (c.is_found_column      = 'Y'  OR x.search_columns      IS NULL)
     --AND (c.is_found_data_type   = 'Y'  OR x.search_data_type    IS NULL)
