cf637072e544d381fc1eacb688f073d0507b6281
Jan Kvetina <jan.kvetina@gmail.com>
2022-03-09 22:31:15 +0100

    Add owner/schema for sequences

--- views/obj_sequences.sql
+++ views/obj_sequences.sql
@@ -1,20 +1,30 @@
 CREATE OR REPLACE VIEW obj_sequences AS
-WITH m AS (
+WITH x AS (
+    SELECT /*+ MATERIALIZE */
+        app.get_owner()         AS owner
+    FROM DUAL
+),
+m AS (
     -- map sequences to tables (based on column name)
     SELECT
         c.table_name,
         MIN(c.column_name)      AS column_name,
         s.sequence_name
-    FROM user_constraints n
-    JOIN user_cons_columns c
-        ON c.constraint_name    = n.constraint_name
-    LEFT JOIN user_tab_columns d
-        ON d.table_name         = c.table_name
+    FROM all_constraints n
+    JOIN x
+        ON n.owner              = x.owner
+    JOIN all_cons_columns c
+        ON c.owner              = n.owner
+        AND c.constraint_name   = n.constraint_name
+    LEFT JOIN all_tab_columns d
+        ON d.owner              = c.owner
+        AND d.table_name        = c.table_name
         AND d.column_name       = c.column_name
         AND d.column_id         = 1
         AND d.data_type         = 'NUMBER'
-    LEFT JOIN user_sequences s
-        ON (
+    LEFT JOIN all_sequences s
+        ON s.sequence_owner     = c.owner
+        AND (
             s.sequence_name     = c.column_name
             OR s.sequence_name  = 'SEQ_' || c.table_name
             OR s.sequence_name  = c.table_name || '_SEQ'
@@ -36,9 +46,11 @@ SELECT
     s.last_number,
     m.table_name,
     m.column_name
-FROM user_sequences s
+FROM all_sequences s
+JOIN x
+    ON x.owner                  = s.sequence_owner
 LEFT JOIN m
-    ON m.sequence_name = s.sequence_name;
+    ON m.sequence_name          = s.sequence_name;
 
 
 
