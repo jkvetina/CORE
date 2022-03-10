1494246f128d1dd64a634a3982b53c18e0eb1163
Jan Kvetina <jan.kvetina@gmail.com>
2022-03-10 22:15:14 +0100

    Render new/removed pages on the fly

--- views/nav_overview.sql
+++ views/nav_overview.sql
@@ -13,7 +13,7 @@ SELECT
     n.order#,
     n.page_group,
     n.page_alias,
-
+    --
     CASE WHEN n.depth IS NOT NULL
         THEN REPLACE(LTRIM(RPAD('-', n.depth * 4), '-'), ' ', '&' || 'nbsp; ') ||
             app.get_page_name (
@@ -38,14 +38,28 @@ SELECT
     n.is_javascript,
     n.javascript,
     n.auth_scheme,
-    n.page_url,
+    --
+    CASE WHEN r.page_id IS NULL THEN n.page_url END AS page_url,
+    --
     n.comments,
     n.allow_changes,
     n.sort_order,
-    n.action,
-    n.action_url
+    --
+    CASE
+        WHEN r.page_id IS NOT NULL
+            THEN app.get_icon('fa-minus-square', 'Remove record from Navigation table')
+        END AS action,
+    --
+    app.get_page_url (
+        in_page_id          => 910,
+        in_app_id           => x.core_app_id,
+        in_names            => 'P' || TO_CHAR(910) || '_REMOVE_PAGE',
+        in_values           => TO_CHAR(n.page_id)
+    ) AS action_url
 FROM nav_overview_mvw n
 CROSS JOIN x
+LEFT JOIN nav_pages_to_remove r
+    ON r.page_id            = n.page_id
 WHERE (
     n.app_id                = x.app_id
     OR (
@@ -57,7 +71,73 @@ WHERE (
             WHERE n.app_id      = x.app_id
         )
     )
-);
+)
+--
+UNION ALL
+SELECT
+    n.app_id,
+    n.page_id,
+    n.parent_id,
+    n.order#,
+    NVL(t.page_root, n.page_id) || ' ' || n.page_group AS page_group,
+    n.page_alias,
+    --
+    n.page_name,
+    n.page_title,
+    n.css_class,
+    n.page_template,
+    --
+    n.is_hidden,
+    n.is_reset,
+    n.is_shared,
+    --
+    CASE WHEN n.page_mode = 'Normal'    THEN NULL ELSE 'Y' END  AS is_modal,
+    CASE WHEN t.javascript IS NOT NULL  THEN 'Y' END            AS is_javascript,
+    t.javascript,
+    --
+    CASE WHEN n.auth_scheme LIKE '%MUST_NOT_BE_PUBLIC_USER%'
+        THEN app.get_icon('fa-check-square', 'MUST_NOT_BE_PUBLIC_USER')
+        ELSE n.auth_scheme
+        END AS auth_scheme,
+    --
+    CASE
+        WHEN t.javascript_target IS NOT NULL
+            THEN t.javascript_target
+            --
+        ELSE app.get_page_url (
+            in_page_id      => n.page_id,
+            in_app_id       => n.app_id
+        )
+        END AS page_url,
+    --
+    n.comments,
+    --
+    NULL AS allow_changes,  -- no changes allowed
+    --
+    NVL(t.page_root, n.page_id) || '.' || TO_CHAR(10000 + (
+        SELECT NVL(MAX(z.r#), 0) AS nearest_r#
+        FROM nav_overview_mvw z
+        WHERE z.app_id          = n.app_id
+            AND z.page_group    = n.page_group
+            AND z.order#        = n.order#
+    )) || '.' || NVL(n.order#, n.page_id) AS sort_order,
+    --
+    app.get_icon('fa-plus-square', 'Create record in Navigation table') AS action,
+    --
+    app.get_page_url (
+        in_page_id         => 910,
+        in_app_id          => app.get_core_app_id(),
+        in_names           => 'P' || TO_CHAR(910) || '_ADD_PAGE',
+        in_values          => TO_CHAR(n.page_id)
+    ) AS action_url
+FROM nav_pages_to_add n
+JOIN x
+    ON x.app_id             = n.app_id
+JOIN apps a
+    ON a.app_id             = n.app_id
+LEFT JOIN nav_overview_mvw t
+    ON t.app_id             = n.app_id
+    AND t.page_id           = n.parent_id;
 --
 COMMENT ON TABLE  nav_overview                  IS '[CORE - DASHBOARD] Enriched navigation overview used also for menu rendering';
 --
