fb702e67027a649518479a90b23a9f4a56dbf0fb
Jan Kvetina <jan.kvetina@gmail.com>
2022-12-11 20:23:15 +0100

    Use MVs in Top navigation

--- database/views/nav_top.sql
+++ database/views/nav_top.sql
@@ -7,31 +7,33 @@ WITH curr AS (
         app.get_page_root()         AS page_root,
         app.get_page_group()        AS page_group,
         app.get_user_id()           AS user_id,
-        app.get_user_name()         AS user_name
+        app.get_user_name()         AS user_name,
+        app.get_session_id()        AS session_id
     FROM DUAL
 )
 SELECT
     CASE WHEN n.parent_id IS NULL THEN 1 ELSE 2 END AS lvl,
     --
     CASE
-        WHEN n.page_id = 100  -- home page
-            THEN REPLACE(n.page_name, '&' || 'ENV_NAME.', app.get_env_name() || ' &' || 'nbsp; ')
         WHEN n.page_id > 0
-            THEN REGEXP_REPLACE(REPLACE(n.page_name, '&' || 'APP_USER.', APEX_ESCAPE.HTML(NVL(curr.user_name, curr.user_id))), '^(&' || 'nbsp; )+', '')
+            THEN REGEXP_REPLACE(
+                REPLACE(
+                REPLACE(p.page_name, '&' || 'ENV_NAME.', app.get_env_name() || ' &' || 'nbsp; '),                
+                '&' || 'APP_USER.', APEX_ESCAPE.HTML(NVL(curr.user_name, curr.user_id))),
+                '^(&' || 'nbsp; )+', '')
         ELSE '</li></ul><ul class="EMPTY"></ul><ul><li style="display: none;">'  -- a trick to split nav menu to left and right
         END AS label,
     --
     CASE
-        WHEN n.javascript IS NOT NULL
-            THEN n.page_url
+        WHEN (p.javascript IS NOT NULL OR n.page_id = 0)
+            THEN '#'
         WHEN n.page_id > 0
             THEN APEX_PAGE.GET_URL (
                 p_application   => n.app_id,
-                p_page          => NVL(n.page_alias, TO_CHAR(n.page_id)),
+                p_page          => NVL(p.page_alias, TO_CHAR(n.page_id)),
                 p_clear_cache   => CASE WHEN n.is_reset = 'Y' THEN n.page_id END,
-                p_session       => CASE WHEN n.page_id != 9999 THEN app.get_session_id() ELSE 0 END
+                p_session       => CASE WHEN n.page_id != 9999 THEN curr.session_id ELSE 0 END
             )
-        ELSE NVL(n.page_url, '#')
         END AS target,
     --
     CASE
@@ -44,8 +46,7 @@ SELECT
     --
     CASE
         WHEN n.page_id > 0
-            THEN n.page_group || ' ' || n.css_class
-            ELSE 'HIDDEN'
+            THEN p.page_group
         END AS attribute01,
     --
     NULL AS attribute02,                        -- prepend link with element
@@ -58,12 +59,12 @@ SELECT
     CASE
         WHEN n.page_id = 9999
             THEN 'Logout'
-            ELSE n.page_title
+            ELSE p.page_title
             END AS attribute04,                 -- a.title
     --
-    n.javascript AS attribute05,                -- javascript action
+    p.javascript AS attribute05,                -- javascript action
     --
-    NULL                                                AS attribute06,     -- badge left
+    NULL                                        AS attribute06,     -- badge left
     --
     CASE WHEN b.badge IS NOT NULL
         THEN '<span class="BADGE">' || b.badge || '</badge>'
@@ -73,15 +74,19 @@ SELECT
     NULL AS attribute09,
     NULL AS attribute10,
     --
-    n.page_group,
-    n.sort_order
-FROM nav_overview n
+    n.r#
+FROM nav_source_sorted_mv n
+JOIN nav_source_pages_mv p
+    ON p.app_id         = n.app_id
+    AND p.page_id       = n.page_id
 CROSS JOIN curr
 LEFT JOIN nav_badges b
     ON b.page_id        = n.page_id
-WHERE n.action          IS NULL
-    AND n.is_hidden     IS NULL
-    AND 'Y'             = nav.is_page_available(n.page_id, n.app_id);
+WHERE n.is_hidden       IS NULL
+    AND (
+        'Y' = nav.is_page_available(p.auth_scheme, p.app_id, p.page_id, p.procedure_name, p.data_type, p.page_argument)
+        OR n.page_id IN (0, 9999)
+    );
 --
 COMMENT ON TABLE nav_top IS '[CORE - DASHBOARD] Navigation view used for rendering top menu';
 --
